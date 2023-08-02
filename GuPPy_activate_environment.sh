b01516bbed2bd8050b1ec95a84a6b93c9e04f485
Venus Sherathiya <32345130+venus-sherathiya@users.noreply.github.com>
2023-08-02 10:51:36 -0500

    change in indentation in bash scripts

--- GuPPy_activate_environment.sh
+++ GuPPy_activate_environment.sh
@@ -2,19 +2,19 @@
 
 # Check if the OS is Linux
 if [[ "$(uname)" == "Linux" ]]; then
-  source "$(conda info --base)/bin/activate" guppy
+	source "$(conda info --base)/bin/activate" guppy
   
 # Check if the OS is macOS
 elif [[ "$(uname)" == "Darwin" ]]; then
-  echo "$(uname)"
-  source "$(conda info --base)/bin/activate" guppy
+	echo "$(uname)"
+	source "$(conda info --base)/bin/activate" guppy
 
 # Check if the OS is Windows (using MSYS or Cygwin)
 elif [[ "$(uname -o 2>/dev/null)" == "Cygwin" || "$(uname -o 2>/dev/null)" == "Msys" ]]; then
-  source "$(conda info --base)/Scripts/activate" guppy
+	source "$(conda info --base)/Scripts/activate" guppy
 
 # If none of the above, assume it's an unknown OS
 else
-  echo "$(uname)"
-  echo "Unknown operating system detected."
+	echo "$(uname)"
+	echo "Unknown operating system detected."
 fi
--- GuPPy_create_environment.sh
+++ GuPPy_create_environment.sh
@@ -2,39 +2,39 @@
 
 # Check if the OS is Linux
 if [[ "$(uname)" == "Linux" ]]; then
-  echo "Linux operating system detected."
-  sudo apt-get update
-  apt-get install wget
-  wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
-  sha256sum Anaconda3-2022.05-Linux-x86_64.sh
-  bash Anaconda3-2022.05-Linux-x86_64.sh
-  rm -rf Anaconda3-2022.05-Linux-x86_64.sh
-  conda_file="./GuPPy_environment_ubuntu.yml"
+	echo "Linux operating system detected."
+	sudo apt-get update
+	apt-get install wget
+	wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+	sha256sum Anaconda3-2022.05-Linux-x86_64.sh
+	bash Anaconda3-2022.05-Linux-x86_64.sh
+	rm -rf Anaconda3-2022.05-Linux-x86_64.sh
+	conda_file="./GuPPy_environment_ubuntu.yml"
   
 # Check if the OS is macOS
 elif [[ "$(uname)" == "Darwin" ]]; then
-  echo "macOS operating system detected."
-  curl -O https://repo.anaconda.com/archive/Anaconda3-2022.05-MacOSX-x86_64.sh
-  sha256sum Anaconda3-2022.05-MacOSX-x86_64.sh
-  bash Anaconda3-2022.05-MacOSX-x86_64.sh
-  rm -rf Anaconda3-2022.05-MacOSX-x86_64.sh
-  # Fetch the Conda YAML file from the specified path
-  conda_file="./GuPPy_environment_mac.yml"
+	echo "macOS operating system detected."
+	curl -O https://repo.anaconda.com/archive/Anaconda3-2022.05-MacOSX-x86_64.sh
+	sha256sum Anaconda3-2022.05-MacOSX-x86_64.sh
+	bash Anaconda3-2022.05-MacOSX-x86_64.sh
+	rm -rf Anaconda3-2022.05-MacOSX-x86_64.sh
+	# Fetch the Conda YAML file from the specified path
+	conda_file="./GuPPy_environment_mac.yml"
 
 # Check if the OS is Windows (using MSYS or Cygwin)
 elif [[ "$(uname -o 2>/dev/null)" == "Cygwin" || "$(uname -o 2>/dev/null)" == "Msys" ]]; then
-  echo "Windows operating system detected."
+	echo "Windows operating system detected."
 
 # If none of the above, assume it's an unknown OS
 else
-  echo "$(uname)"
-  echo "Unknown operating system detected."
+	echo "$(uname)"
+	echo "Unknown operating system detected."
 fi
 
 # Check if the Conda YAML file exists
 if [ ! -f "$conda_file" ]; then
-  echo "Conda YAML file not found in the specified path."
-  exit 1
+	echo "Conda YAML file not found in the specified path."
+	exit 1
 fi
 
 # Extract the environment name from the Conda YAML file
@@ -42,8 +42,8 @@ env_name=$(grep -e 'name: ' "$conda_file" | awk '{print $2}')
 
 # Check if the Conda environment with the same name exists
 if conda env list | grep -q -w "$env_name"; then
-  echo "Conda environment '$env_name' already exists. Removing it..."
-  conda remove -n "$env_name" --all
+	echo "Conda environment '$env_name' already exists. Removing it..."
+	conda remove -n "$env_name" --all
 fi
 
 # Create a new Conda environment using the fetched YAML file
