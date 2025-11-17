aa4340d97f554c7434a73a58a02fc4ee994f33dc
pauladkisson <paul.wesley.adkisson@gmail.com>
2025-11-17 10:59:52 -0800

    Moved readtsq to tdt_step2.py.

--- src/guppy/saveStoresList.py
+++ src/guppy/saveStoresList.py
@@ -21,6 +21,8 @@ import pandas as pd
 import panel as pn
 from numpy import float32, float64, int32, int64, uint16
 
+from guppy.tdt_step2 import readtsq
+
 # hv.extension()
 pn.extension()
 
@@ -84,25 +86,6 @@ def check_header(df):
             pass
 
     return arr, check_float
-
-
-# function to read 'tsq' file
-def readtsq(filepath):
-    names = ("size", "type", "name", "chan", "sort_code", "timestamp", "fp_loc", "strobe", "format", "frequency")
-    formats = (int32, int32, "S4", uint16, uint16, float64, int64, float64, int32, float32)
-    offsets = 0, 4, 8, 12, 14, 16, 24, 24, 32, 36
-    tsq_dtype = np.dtype({"names": names, "formats": formats, "offsets": offsets}, align=True)
-    path = glob.glob(os.path.join(filepath, "*.tsq"))
-    if len(path) > 1:
-        logger.error("Two tsq files are present at the location.")
-        raise Exception("Two tsq files are present at the location.")
-    elif len(path) == 0:
-        return 0
-    else:
-        path = path[0]
-    tsq = np.fromfile(path, dtype=tsq_dtype)
-    df = pd.DataFrame(tsq)
-    return df
 
 
 # function to show GUI and save
---
+++ src/guppy/tdt_step2.py
@@ -0,0 +1,26 @@
+import glob
+import logging
+import os
+import numpy as np
+from numpy import float32, float64, int32, int64, uint16
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+# function to read 'tsq' file
+def readtsq(filepath):
+    names = ("size", "type", "name", "chan", "sort_code", "timestamp", "fp_loc", "strobe", "format", "frequency")
+    formats = (int32, int32, "S4", uint16, uint16, float64, int64, float64, int32, float32)
+    offsets = 0, 4, 8, 12, 14, 16, 24, 24, 32, 36
+    tsq_dtype = np.dtype({"names": names, "formats": formats, "offsets": offsets}, align=True)
+    path = glob.glob(os.path.join(filepath, "*.tsq"))
+    if len(path) > 1:
+        logger.error("Two tsq files are present at the location.")
+        raise Exception("Two tsq files are present at the location.")
+    elif len(path) == 0:
+        return 0
+    else:
+        path = path[0]
+    tsq = np.fromfile(path, dtype=tsq_dtype)
+    df = pd.DataFrame(tsq)
+    return df
